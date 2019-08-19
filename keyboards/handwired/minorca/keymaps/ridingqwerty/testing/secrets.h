73d93a655448cbd74b320ed49aa98eb2cafd2e6c
George Koenig <gkoenig@coredial.com>
2019-08-19 00:14:47 -0400

    adding handwired/minorca keymap

---
+++ keyboards/handwired/minorca/keymaps/ridingqwerty/testing/secrets.h
@@ -0,0 +1,25 @@
+// add me to `.git/info/exclude`
+#define SS_SECRET "xmas is gay\n"
+#define SS_RUSTY "ru5tygentoo\n"
+#define SS_FUEL "ru5tyB1ke$\n"
+#define SS_VAXIS "siX3ci0vc0re\n"
+#define SS_C0RE "c0re7667\n"
+#define SS_MUNKY "monkeyshit\n"
+#define SS_AR1ST "Ar1st0tl3!\n"
+#define SS_OS_LAB "4Sh4m30fD3V\n"
+#define SS_OS_LAB "4Sh4m30fD3V\n"
+#define SS_SYSNOC "sySt3msk@n0c\n"
+
+
+/*
+###### .git/info/exclude
+```
+# git ls-files --others --exclude-from=.git/info/exclude
+# Lines that start with '#' are comments.
+# For a project mostly in C, the following would be a good set of
+# exclude patterns (uncomment them if you want to use them):
+# *.[oa]
+# *~
+/keyboards/xd75/keymaps/colinta/secrets.h
+```
+*/
