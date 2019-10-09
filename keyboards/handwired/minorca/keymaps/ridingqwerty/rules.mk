78b2146ef5f68667a78da5f24ae3f56a6b18994e
George Koenig <gkoenig@coredial.com>
2019-10-09 16:07:01 -0400

    repo fixup

--- keyboards/handwired/minorca/keymaps/ridingqwerty/rules.mk
+++ keyboards/handwired/minorca/keymaps/ridingqwerty/rules.mk
@@ -1,4 +1,4 @@
-
+BOOTLOADER = halfkay
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in 
@@ -13,11 +13,11 @@ NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: ht
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
-UNICODEMAP_ENABLE = yes     # Unicode
+UNICODE_ENABLE = yes         # Unicode
+#UNICODEMAP_ENABLE = yes     # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
-TAP_DANCE_ENABLE = yes      # Enable Tapdance routines
+TAP_DANCE_ENABLE = no      # Enable Tapdance routines
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
--- users/ridingqwerty/ridingqwerty.c
+++ users/ridingqwerty/ridingqwerty.c
@@ -1,1 +1,44 @@
 #include "ridingqwerty.h"
+#ifdef CONSOLE_ENABLE
+//#    include "printf.h"
+#endif
+
+__attribute__((weak))
+void matrix_init_keymap(void) {}
+
+void matrix_init_user(void) {
+#if defined(UNICODE_ENABLE) || defined(UNICODEMAP_ENABLE)
+    set_unicode_input_mode(UC_LNX);
+#endif
+}
+
+__attribute__((weak))
+void matrix_scan_keymap(void) {
+}
+
+
+void matrix_scan_user(void) {
+
+}
+
+/*
+__attribute__((weak))
+void keyboard_post_init_keymap(void) {}
+
+void keyboard_post_init_user(void) {
+#if defined(CONSOLE_ENABLE)
+    debug_enable = true;
+    debug_matrix = true;
+#endif
+}
+*/
+
+__attribute__((weak))
+layer_state_t   layer_state_set_keymap(layer_state_t state) { return state; }
+
+layer_state_t	layer_state_set_user(layer_state_t state) {
+    //state = update_tri_layer_state(state, _SYMBOL, _NUMBER, _SECRET);
+    state = update_tri_layer_state(state, _EDITOR, _DEBUG, _SECRET);
+    return state;
+}
+
