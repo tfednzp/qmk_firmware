422369900d4cfc6646afa9e56cd1e39bee367749
George Koenig <gkoenig@coredial.com>
2020-01-17 13:15:20 -0500

    testing

--- users/ridingqwerty/config.h
+++ users/ridingqwerty/config.h
@@ -3,6 +3,8 @@
 #define MACRO_TIMER 5
 // testing
 #define IGNORE_MOD_TAP_INTERRUPT // rolling R3
-//#define TAPPING_FORCE_HOLD // hopefully fix ":0" issue with numpad layer from `;`
 // testing
-#define TAPPING_FORCE_HOLD_PER_KEY
+//#define TAPPING_FORCE_HOLD_PER_KEY
+//#define TAPPING_FORCE_HOLD
+#define ONESHOT_TAP_TOGGLE 2
+#define TAPPING_TOGGLE 2
